c0e7250c27bcca7097ab4b4e39b33c43d7f848e8
Karun-Dixit <karundixit5@gmail.com>
2025-07-29 18:58:01 +0545

    Fully Completed

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5174,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '.cert/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '.cert/cert.pem')),
+    },
+  },
+})
